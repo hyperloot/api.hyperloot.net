a1d4b75446491f3a80fce65e855abd5f9a148a0a
Valery Vaskabovich <valery.voskobovich@gmail.com>
2018-10-24 08:06:32 -0700

    Added method for the user, configured datasource

--- server/models/hyperloot-user.js
+++ server/models/hyperloot-user.js
@@ -1,5 +1,28 @@
 'use strict';
 
 module.exports = function(Hyperlootuser) {
+	Hyperlootuser.canRegister = function(email, callback) {
+		Hyperlootuser.findOne({
+			where: {
+				email: email
+			}}, function(err, user) {
+				if (err) {
+					callback(false)
+				} else {
+					callback(true)
+				}
+			});
+	}
 
+	Hyperlootuser.remoteMethod('canRegisterEmail', {
+		'http': {
+			'path': '/canRegisterEmail', 'verb': 'get' 
+		},
+		'accepts': [
+			{ 'arg': 'email', 'type': 'string', 'required': true }
+		],
+		'returns': [
+			{ 'arg': 'canRegister', 'type': 'boolean', 'root': true }
+		]
+	});
 };
