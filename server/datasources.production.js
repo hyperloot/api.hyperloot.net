f383ed0d5be6c2ac8fa7eaae240649a90999359e
Valery Vaskabovich <valery.voskobovich@gmail.com>
2018-12-02 23:32:48 -0800

    Fixed datasources configuration

--- server/datasources.production.js
+++ server/datasources.production.js
@@ -3,6 +3,7 @@
 module.exports = {
   hlapi: {
     connector: 'mongodb',
+    name: 'hlapi',
     user: process.env.MONGODB_USER,
     password: process.env.MONGODB_PASSWORD,
     url: process.env.MONGODB_URI,
